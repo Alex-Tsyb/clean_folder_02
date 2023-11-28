5bc767fad7908877658457f3c0605bb194d45200
Oleksii Tsybrovskyi <tsybrovsky@gmail.com>
2023-11-28 22:00:39 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="clean_folder",
+    version="1",
+    description="Clean You folder",
+    author="Alex_Tsybr",
+    license="MIT",
+    packages=find_namespace_packages(),
+    entry_points={"console_scripts": ["clean-folder = clean_folder.clean:main"]},
+)
